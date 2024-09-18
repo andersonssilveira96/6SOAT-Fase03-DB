b0cc7072cef5410b2ee7a8b1d0838d8258c31e45
Anderson Souza <andersonssilveira96@gmail.com>
2024-09-17 21:16:46 -0300

    Adding new folder

---
+++ src/rds.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "db_sg" {
+  name_prefix = "db_sg-"
+  ingress {
+    from_port   = 0
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "db_sg" {
+  engine                 = "postgres"
+  engine_version         = "14"
+  db_name                = var.db-name
+  identifier             = var.db-name
+  instance_class         = "db.t3.medium"
+  allocated_storage      = 20
+  publicly_accessible    = true
+  username               = var.db-username
+  password               = var.db-password
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  skip_final_snapshot    = true
+
+  tags = {
+    Name = var.db-name
+  }
+}
---
+++ src/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default  = "us-east-1"
+}
+
+variable "db-username" {
+  default  = "postgres"
+}
+
+variable "db-password" {
+  default = "postgres"
+}
+
+variable "db-name" {
+  default = "techchallenge"
+}
